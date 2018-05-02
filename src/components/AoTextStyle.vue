f660db37b1bdd640b3db0c8148b18b0e9483ccfd
Daniel Busheikin <daniel.busheikin@ampleorganics.com>
2018-05-02 17:27:27 -0400

    Create TextSyle componentt

---
+++ src/components/AoTextStyle.vue
@@ -0,0 +1,44 @@
+<template>
+  <span
+    :class="[computedClasses]"
+  >
+    <slot/>
+  </span>
+</template>
+
+<script>
+import { filterClasses } from './utils/component_utilities.js'
+export default {
+  props: {
+    error: {
+      type: Boolean,
+      default: false
+    },
+
+    small: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    computedClasses () {
+      const computedClasses = {
+        'ao-text--error': this.error,
+        'ao-text--small': this.small
+      }
+      return filterClasses(computedClasses)
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  .ao-text--error {
+    color: $font-color-error;
+  }
+
+  .ao-text--small {
+    font-size: $font-size-xs;
+  }
+</style>
