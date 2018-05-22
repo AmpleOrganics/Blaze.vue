e9b820935452441527107b386b7665bd54ab97e6
Zain Wania <zain.wania@ampleorganics.com>
2018-05-22 13:36:39 -0400

    disables pagination button once first page or last page

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -115,7 +115,10 @@
     </ao-card>
 
     <ao-card :title="'Hi I am a Table!'">
-      <!-- <ao-paginate slot="card-header-toolbar" @paginate="paginate"/> -->
+      <ao-paginate
+        slot="card-header-toolbar"
+        :total-pages="3"
+        @paginate="paginate"/>
       <ao-table :headers="columnHeaders" @sortTable="sortTable">
         <tr v-for="user in displayUsers" :key="user.id">
           <td>{{ user.id }}</td>
@@ -208,7 +211,8 @@ export default {
       loading: false,
       showModal: false,
       showAlert: false,
-      routes: this.$router.options.routes
+      routes: this.$router.options.routes,
+      page: 1
     }
   },
 
@@ -261,6 +265,8 @@ export default {
     },
     paginate (page, direction) {
       this.userInfo.page = page
+      this.page = page
+      console.log(this.page)
     }
   }
 }
