9a5c89d41139a7abda88851604f0d90c914d2a3d
Zain Wania <zain.wania@ampleorganics.com>
2018-07-05 17:44:08 -0400

    button docs

---
+++ docs/.vuepress/components/AoFileUpload.vue
@@ -0,0 +1,52 @@
+<template>
+  <label
+    v-show="showLabel"
+    class="ao-form-group">
+    {{ label }}
+    <input
+      :class="[{'ao-form-control--invalid': invalid }, 'ao-form-control']"
+      :name="name"
+      :disabled="disabled"
+      type="file"
+      @change="updateFile($event.target.files)">
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+
+    showLabel: {
+      type: Boolean,
+      default: true
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+
+    invalid: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    updateFile (value) {
+      this.$emit('change', value[0])
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+@import '../shared-input-styles.scss';
+@include shared-input-styles;
+
+</style>
---
+++ docs/components/README.md
@@ -0,0 +1,7 @@
+---
+sidebarDepth: 0
+---
+
+# Overview
+
+Blaze component library for all of your UI building needs
