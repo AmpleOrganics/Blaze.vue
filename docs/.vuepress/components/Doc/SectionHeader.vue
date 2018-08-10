f3aeed8a23463690f68355cd5e6bcf240bae645c
Lauren Herdman <lauren.herdman@ampleorganics.com>
2018-08-10 12:43:29 -0400

    Add section header documentation

---
+++ docs/.vuepress/components/Doc/SectionHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="component-example">
+      <ao-section-header
+        :title="title"
+        :subtitle="subtitle">
+      </ao-section-header>
+    </div>
+    <div class="component-controls">
+      <div class="component-controls__group">
+        <ao-input
+          v-model="title"
+          :type="'text'"
+          :label="'Section Header Title'"
+        />
+      </div>
+      <div class="component-controls__group">
+        <ao-input
+          v-model="subtitle"
+          :type="'text'"
+          :label="'Section Header SubTitle'"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: 'Section Header Title',
+      subtitle: 'This is a subtitle'
+    }
+  }
+}
+</script>
