7bef14b0dd06ae56615f5d61200d790d51cdeb4a
Tessa Kruger <tessa.kruger@ampleorganics.com>
2018-08-13 15:34:20 -0400

    Clean up modal doc

--- docs/.vuepress/components/Doc/Modal.vue
+++ docs/.vuepress/components/Doc/Modal.vue
@@ -5,7 +5,9 @@
       <ao-modal
         v-if="showModal"
         :header-text="headerText"
-        @modalClose="toggleModal">
+        @modalClose="toggleModal"
+        :destructive="selectedRadio === 'destructive'"
+        :caution="selectedRadio === 'caution'">
         <div slot="modal-body">
           <p>{{ contentText }}</p>
         </div>
@@ -36,6 +38,17 @@
         />
       </div>
     </div>
+    <div class="component-controls">
+      <div class="component-controls__group">
+        <ao-radio
+          v-for="radio in radios"
+          :key="radio.value"
+          :val="radio.value"
+          :name="radio.name"
+          :label="radio.name"
+          v-model="selectedRadio" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,8 +58,14 @@ export default {
   data() {
     return {
       showModal: false,
-      headerText: 'Header Text',
-      contentText: 'I am context'
+      headerText: 'This is the modal title',
+      contentText: 'And I live in the body of the modal',
+      radios: [
+        { name: 'default', value: 'default' },
+        { name: 'destructive', value: 'destructive' },
+        { name: 'caution', value: 'caution' }
+      ],
+      selectedRadio: 'default'
     }
   },
 
