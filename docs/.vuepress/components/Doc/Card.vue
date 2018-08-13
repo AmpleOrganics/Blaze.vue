69318737463cafde22d554f00a4b2f19b9977ad8
Tessa Kruger <tessa.kruger@ampleorganics.com>
2018-08-13 14:00:34 -0400

    Clean up card component doc

--- docs/.vuepress/components/Doc/Card.vue
+++ docs/.vuepress/components/Doc/Card.vue
@@ -10,8 +10,7 @@
           primary>
           Action 1
         </ao-button>
-        <ao-button
-          v-show="buttonTwo">
+        <ao-button>
           Action 2
         </ao-button>
       </ao-card>
@@ -20,14 +19,11 @@
       <ao-input
         v-model="titleText"
         :type="'text'"
-        label="Card Title" />
+        label="Card Title Text" />
       <ao-input
         v-model="slotText"
         :type="'text'"
-        label="Slot Elements" />
-      <ao-checkbox
-        v-model="buttonTwo"
-        :checkbox-label="buttonActionLabel" />
+        label="Card Content Text" />
     </div>
   </div>
 </template>
@@ -37,14 +33,7 @@ export default {
   data () {
     return {
       titleText: 'Title goes here',
-      slotText: 'She sells seashells by the sea shore',
-      buttonTwo: false
-    }
-  },
-
-  computed: {
-    buttonActionLabel () {
-      return this.buttonTwo ? 'Remove button component' : 'Add another button component'
+      slotText: 'She sells seashells by the sea shore'
     }
   }
 }
