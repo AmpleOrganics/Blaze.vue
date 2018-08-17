a48afd1557c6676367b57c551d822096614f3c99
Zain Wania <zain.wania@ampleorganics.com>
2018-08-17 10:52:26 -0400

    breadcrumb component

---
+++ docs/.vuepress/components/Doc/Breadcrumbs.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="component-example">
+    <ao-breadcrumbs :paths="paths"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      paths: [
+        { name: 'Home', path: '/' },
+        { name: 'Somewhere', path: '/components/Badge.html' },
+        { name: 'Current', active: true }
+      ]
+    }
+  }
+}
+</script>
