3f303be661d940b069aae9e684819d85b87435a6
Daniel Busheikin <daniel.busheikin@ampleorganics.com>
2018-09-28 18:07:57 -0400

    Style Blaze docs

--- docs/layout/LayoutSidebar.vue
+++ docs/layout/LayoutSidebar.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="layoutsidebar">
-    <h5>Components</h5>
-    <ul>
-      <li
-        v-for="route in componentRoutes"
-        :key="route.name"
-        class="layoutsidebar__item">
-        <router-link
-          :to="route.name"
-          class="layoutsidebar__item-link">{{ route.meta.title }}</router-link>
-      </li>
-    </ul>
-  </div>
+  <ao-card class="layoutsidebar">
+    <nav>
+      <ao-heading
+        card-section-heading
+        text="Components"/>
+      <ul>
+        <li
+          v-for="route in componentRoutes"
+          :key="route.name"
+          class="layoutsidebar__item">
+          <router-link
+            :to="route.name"
+            class="layoutsidebar__item-link">{{ route.meta.title }}</router-link>
+        </li>
+      </ul>
+    </nav>
+  </ao-card>
 </template>
 
 <script>
@@ -28,6 +32,7 @@ export default {
 
 <style lang="scss" scoped>
 .layoutsidebar {
+
   &__item {
     margin: 0.5rem 0;
   }
