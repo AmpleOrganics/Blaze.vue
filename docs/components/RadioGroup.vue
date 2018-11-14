5a1a2d11a5239eb9d2472c0a2b76ecce2c40629f
Daniel Busheikin <daniel.busheikin@ampleorganics.com>
2018-11-14 15:11:33 -0500

    CSS style

--- docs/components/RadioGroup.vue
+++ docs/components/RadioGroup.vue
@@ -8,12 +8,8 @@
     <div slot="example">
       <div class="component-example">
         <div class="component-example__display"> My favourite character is {{ selectedRadio }} </div>
-        <ao-radio
-          v-for="radio in radios"
-          :key="radio.value"
-          :val="radio.value"
-          :name="radio.name"
-          :label="radio.name"
+        <ao-radio-group
+          :options="radios"
           v-model="selectedRadio"/>
       </div>
     </div>
@@ -42,9 +38,7 @@ export default {
       ...RadioGroupDocumentation,
       radios: [
         { name: 'Tandy', value: 'Tandy' },
-        { name: 'Carol', value: 'Carol' },
-        { name: 'Gail', value: 'Gail' },
-        { name: 'Erica', value: 'Erica' },
+        { name: 'Other Guy', value: 'Other' },
         { name: 'Todd', value: 'Todd' }
       ],
       selectedRadio: 'Tandy'
--- src/components/AoRadioGroup.vue
+++ src/components/AoRadioGroup.vue
@@ -2,10 +2,9 @@
   <div class="ao-radio-group">
     <label
       v-for="option in options"
-      :class="[ {'ao-radio-group--option__active': isActive(option.value)}, 'ao-radio-group--option']"
+      :class="[ {'ao-radio-group__option--active': isActive(option.value)}, 'ao-radio-group__option']"
       :key="option.value">
       <input
-        v-show="false"
         v-bind="$attrs"
         :value="option.value"
         :checked="currentValue === option.value"
@@ -57,26 +56,58 @@ export default {
 <style lang="scss">
 .ao-radio-group {
 
-  &--option {
-    border-style: solid;
-    border-width: 2px;
-    border-color: $color-primary;
-    color: $color-primary;
-    padding: 5px;
+  &__option {
+    display: inline-flex;
+    align-items: center;
+    margin-bottom: 0;
+    text-align: center;
+    font-weight: $font-weight-bold;
+    font-family: $font-family-primary;
+    border: $input-border-width solid transparent;
+    border-right-width: 0;
+    padding: $spacer-micro $spacer-sm;
+    font-size: $font-size-base;
+    line-height: $line-height-base;
+    min-height: $input-height-base;
+    box-shadow: $shadow-subtle;
+    cursor: pointer;
 
-    &__active {
-      background-color: $color-primary;
+    background-color: $color-white;
+    border-color: $color-gray-50;
+    color: $font-color-base;
+
+    &:active {
+      box-shadow: $shadow-inset;
+    }
+
+    &:active, &:hover:not([disabled]) {
+      background-color: darken($color-white, 3%);
+      border-color: darken($color-gray-50, 3%);
+      color: darken($font-color-base, 3%);
+    }
+
+    &--active,
+    &--active:active,
+    &--active:hover:not([disabled]) {
+      background-color: $color-success;
+      box-shadow: $shadow-none;
       color: white;
+    }
+
+    & > input {
+      position: absolute;
+      opacity: 0;
     }
   }
   :first-of-type{
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
+    border-top-left-radius: $border-radius-base;
+    border-bottom-left-radius: $border-radius-base;
   }
 
   :last-of-type {
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
+    border-top-right-radius: $border-radius-base;
+    border-bottom-right-radius: $border-radius-base;
+    border-right-width: 1px;
   }
 }
 </style>
