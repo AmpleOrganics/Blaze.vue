a87b1147158f5d25bf79bee7181a00f0d5fe314e
Lauren Herdman <lauren.herdman@ampleorganics.com>
2019-01-22 16:19:48 -0500

    update pagination to the new version of pagination

--- src/components/AoPaginate.vue
+++ src/components/AoPaginate.vue
@@ -1,19 +1,56 @@
 <template>
-  <div class="ao-pagination">
-    <a
-      :disabled="page === 1"
-      class="ao-pagination__button"
-      @click="changePage('prev')"
+  <div class="ao-paginate">
+    <button
+      v-show="currentPage !== 1"
+      class="ao-paginate__button"
+      @click="changePage(currentPage - 1)"
     >
-      &laquo;
-    </a>
-    <a
-      :disabled="page === totalPages"
-      class="ao-pagination__button"
-      @click="changePage('next')"
+      <i class="left-chevron ao-paginate__chevron" />
+    </button>
+    <button
+      v-if="totalPages > 6"
+      :class="{ 'ao-paginate__button--active': isCurrentPage(1) }"
+      class="ao-paginate__button"
+      @click="changePage(1)"
     >
-      &raquo;
-    </a>
+      1
+    </button>
+    <span
+      v-if="firstDots"
+      class="ao-paginate__first-dots"
+    >
+      ...
+    </span>
+    <button
+      v-for="page in displayPages"
+      :key="page"
+      :class="{ 'ao-paginate__button--active': isCurrentPage(page) }"
+      class="ao-paginate__button ao-paginate__page"
+      @click="changePage(page)"
+    >
+      {{ page }}
+    </button>
+    <span
+      v-if="lastDots"
+      class="ao-paginate__last-dots"
+    >
+      ...
+    </span>
+    <button
+      v-show="totalPages > 6"
+      :class="{ 'ao-paginate__button--active': isCurrentPage(totalPages) }"
+      class="ao-paginate__button"
+      @click="changePage(totalPages)"
+    >
+      {{ totalPages }}
+    </button>
+    <button
+      v-if="currentPage !== totalPages"
+      class="ao-paginate__button"
+      @click="changePage(currentPage + 1)"
+    >
+      <i class="right-chevron ao-paginate__chevron" />
+    </button>
   </div>
 </template>
 
@@ -23,92 +60,86 @@ export default {
     totalPages: {
       type: Number,
       default: 1
-    }
-  },
-
-  data () {
-    return {
-      page: 1
+    },
+    currentPage: {
+      type: Number,
+      default: 1
     }
   },
 
   computed: {
-    nextConditions () {
-      return (this.totalPages && this.page < this.totalPages) || !this.totalPages
+    displayPages () {
+      if (this.totalPages > 6) {
+        if (this.currentPage < 4) {
+          return [2, 3, 4]
+        } else if (this.currentPage > this.totalPages - 3) {
+          return [this.totalPages - 3, this.totalPages - 2, this.totalPages - 1]
+        } else {
+          return [this.currentPage - 1, this.currentPage, this.currentPage + 1]
+        }
+      } else {
+        return this.totalPages
+      }
+    },
+
+    isCurrentPage () {
+      return page => this.currentPage === page
+    },
+
+    firstDots () {
+      return (this.currentPage > this.totalPages - 3 || this.currentPage >= 4) && this.totalPages > 6
+    },
+
+    lastDots () {
+      return (this.currentPage < 4 || this.currentPage <= this.totalPages - 3) && this.totalPages > 6
     }
   },
 
   methods: {
-    changePage (direction) {
-      const page = this.page || 1
-
-      if (direction === 'next') {
-        if (this.nextConditions) {
-          this.page += 1
-        }
-      } else if (direction === 'prev') {
-        // set this.page as null if you can't go back at all
-        this.page = this.page <= 1 ? 1 : (this.page - 1)
-      }
-      // if this.page is null then don't do anything at all
-      // handy guard against api calls/more expensive pagination actions
-      if (this.page &&
-        this.page !== page &&
-        (!this.totalPages || this.page <= this.totalPages)
-      ) {
-        this.$emit('paginate', this.page, direction)
-      }
+    changePage (page) {
+      this.$emit('update:currentPage', page)
     }
   }
 }
 </script>
 
-<style lang='scss'>
-.ao-pagination {
-  display: flex;
-  width: 88px;
-
+<style lang="scss">
+.ao-paginate {
   &__button {
-    height: $input-height-base;
-    width: $input-height-base;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    color: $color-gray-10;
-    border: 1px solid $input-border-color;
-    background: $color-white;
-    z-index: $zindex-base;
-    user-select: none;
-    font-weight: $font-weight-bold;
-
-    &[disabled] {
-      cursor: not-allowed !important;
-      opacity: 0.65;
-      filter: alpha(opacity=65);
-      box-shadow: none;
-      color: $color-gray-60;
-      background: $color-white !important;
-      z-index: $zindex-base - 1;
-    }
-
-    &:first-child {
-      border-radius: $border-radius-base 0 0 $border-radius-base;
-      border-right: 0;
-    }
-
-    &:last-child {
-      border-radius: 0 $border-radius-base $border-radius-base 0;
-    }
+    border-radius: $border-radius-base;
+    border: 1px solid $color-gray-60;
+    height: 2rem;
+    min-width: 2rem;
+    padding-right: $spacer-xs;
+    padding-left: $spacer-xs;
+    margin: $spacer-micro/2;
+    font-family: $font-family-primary;
+    font-size: $font-size-sm;
+    color: $font-color-base;
 
     &:hover {
-      text-decoration: none;
+      background-color: darken($color-white, 3%);
+      border-color: darken($color-gray-60, 3%);
+      color: darken($font-color-base, 3%);
     }
 
-    &:hover:not([disabled]) {
-      background: $color-gray-90;
-      color: $color-gray-20;
-      cursor: pointer;
+    &--active,
+    &--active:hover {
+      background: $color-gray-40;
+      color: $color-white;
+      padding-right: $spacer-xs;
+      padding-left: $spacer-xs;
+      font-size: $font-size-sm;
+      min-width: 2rem;
+      border: 1px solid $color-gray-40;
+      cursor: default;
     }
+  }
+
+  &__chevron {
+    font-size: $font-size-base;
+    display: flex;
+    align-items: center;
   }
 }
 </style>
