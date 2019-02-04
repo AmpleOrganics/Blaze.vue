2623432d4d78a107351121d22400b7c25071d098
Tessa Kruger <tessa.kruger@ampleorganics.com>
2019-02-04 15:59:01 -0500

    Toggle new header properties, fixup styling

--- docs/components/TableCell.js
+++ docs/components/TableCell.js
@@ -2,23 +2,25 @@ export default {
   header: 'Table Cell',
   description: 'This styles and sizes inputs and buttons consistently.',
   snippet:
-`<ao-table-cell content="button" >
-      <ao-tooltip text="Archive">
-        <ao-button>
-          <i class="md-icon__archive" />
-        </ao-button>
-      </ao-tooltip>
-      <ao-tooltip text="Download PDF">
-        <ao-button>
-          <i class="md-icon__get_app" />
-        </ao-button>
-      </ao-tooltip>
-      <ao-tooltip text="Actions">
-        <ao-button>
-          <i class="md-icon__more_horiz" />
-        </ao-button>
-      </ao-tooltip>
-    </ao-table-cell>`,
+`
+<ao-table-cell content="button" >
+  <ao-tooltip text="Archive">
+    <ao-button>
+      <i class="md-icon__archive" />
+    </ao-button>
+  </ao-tooltip>
+  <ao-tooltip text="Download PDF">
+    <ao-button>
+      <i class="md-icon__get_app" />
+    </ao-button>
+  </ao-tooltip>
+  <ao-tooltip text="Actions">
+    <ao-button>
+      <i class="md-icon__more_horiz" />
+    </ao-button>
+  </ao-tooltip>
+</ao-table-cell>
+    `,
   apiRows: [
     { name: 'content', type: 'String', default: 'null', description: 'This prop will style either a button or an input appropriately.' },
     { name: 'alignRight', type: 'Boolean', default: 'false', description: 'This prop will align the text in the table cell to the right.' }
