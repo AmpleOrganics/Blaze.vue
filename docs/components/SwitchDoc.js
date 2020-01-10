02925f772177bef3be746b80218dd848dea67bc9
Justen Morgan <justen.morgan@ampleorganics.com>
2020-01-10 10:47:09 -0500

    AoSwitch implementation

---
+++ docs/components/SwitchDoc.js
@@ -0,0 +1,19 @@
+export default {
+  header: 'Switch',
+  description: 'This is a customizable switch component.',
+  snippet:
+        `<ao-switch
+  switch-label="Label"
+  :show-label="true"
+  :switch-value="true"
+  info-text="This is some informative text"
+/>`,
+  apiRows: [
+    { name: 'value', type: '[Array, Boolean, Number, Object]', default: 'null', description: 'Defines the value associated with the input (this is also the value that is sent on submit).' },
+    { name: 'switchValue', type: '[String, Number, Boolean, Object], required', default: 'true', description: 'Defines the value associated with the input.' },
+    { name: 'showLabel', type: 'Boolean', default: 'true', description: 'Hide or unhide the text label.' },
+    { name: 'switchLabel', type: 'String, required', default: '-', description: 'Text label next to the switch.' },
+    { name: 'infoText', type: 'string', default: 'null', description: 'For instructional UI copy pertaining to the switch option' },
+    { name: 'disabled', type: 'Boolean', default: 'false', description: 'Disables interaction with the component.' }
+  ]
+}
---
+++ tests/unit/AoSwitch.spec.js
@@ -0,0 +1,79 @@
+import { mount } from '@vue/test-utils'
+import Switch from '@/components/AoSwitch.vue'
+
+describe('Switch', () => {
+  it('create', () => {
+    const switchComponent = mount(Switch, {
+      propsData: {
+        switchLabel: 'test',
+        switchValue: 'value'
+      }
+    })
+    expect(switchComponent.text()).toBe('test')
+    expect(switchComponent.classes()).toContain('ao-switch')
+  })
+
+  it('disabled', () => {
+    const switchComponent = mount(Switch, {
+      propsData: {
+        switchLabel: 'test0',
+        switchValue: 'value',
+        disabled: true
+      }
+    })
+    expect(switchComponent.text()).toBe('test0')
+    expect(switchComponent.contains(':disabled')).toBe(true)
+  })
+
+  it('showLabel', () => {
+    const switchComponent = mount(Switch, {
+      propsData: {
+        switchLabel: 'test0',
+        switchValue: 'value',
+        showLabel: false
+      }
+    })
+    expect(switchComponent.find('.ao-switch__label-text').isVisible()).toBe(false)
+  })
+
+  it('emit input', () => {
+    const switchComponent = mount(Switch, {
+      propsData: {
+        switchLabel: 'test',
+        switchValue: 'value'
+      }
+    })
+
+    // mark checked
+    switchComponent.find('input').trigger('click')
+    expect(switchComponent.emitted().input[0][0]).toBe(true)
+
+    // mark unchecked
+    switchComponent.find('input').trigger('click')
+    expect(switchComponent.emitted().input[1][0]).toBe(false)
+  })
+
+  it('emit blur', () => {
+    const switchComponent = mount(Switch, {
+      propsData: {
+        switchLabel: 'test',
+        switchValue: 'value'
+      }
+    })
+
+    switchComponent.find('input').trigger('blur')
+    expect(switchComponent.emitted().blur).toBeTruthy()
+  })
+
+  it('emit focus', () => {
+    const switchComponent = mount(Switch, {
+      propsData: {
+        switchLabel: 'test',
+        switchValue: 'value'
+      }
+    })
+
+    switchComponent.find('input').trigger('focus')
+    expect(switchComponent.emitted().focus).toBeTruthy()
+  })
+})
